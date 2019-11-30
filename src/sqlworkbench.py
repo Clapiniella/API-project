2eb385daf54ec45b9a057e32106e4ca98acbc51c
Clara <clapiniella@gmail.com>
2019-11-30 17:39:32 +0100

    heroku loves sudoku

--- src/sqlworkbench.py
+++ src/sqlworkbench.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import sqlalchemy as db
 import getpass
-import psycopg2
 import json
 
 password = getpass.getpass("Insert your mysql root password: ")
@@ -9,8 +8,10 @@ engine = db.create_engine('mysql+pymysql://root:{}@localhost/api-project'.format
 print("Connected to server!")
 
 def populatetable(jdaughter):
-    query= "INSERT INTO {} VALUES {};"
+    query= "INSERT INTO {} VALUES {}"
+
     with engine.connect() as con:
+
         with open(jdaughter) as f:
             chats_json = json.load(f)
         
@@ -20,36 +21,36 @@ def populatetable(jdaughter):
         for user in users:
             q = query.format('users (idUser, userName)',"({}, '{}')".format(user[0],user[1]),'users.idUser')
             print(q)
-        try:
-            con.execute(q)
-            #Get Response
-            id = con.fetchone()[0]
-            print(f"value inserted: {id}")
-        except:
-            print("At least I tried")
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
 
         for chat in chats:
             q = query.format('chats(idChat)',"({})".format(chat),'chats.idChat')
             print(q)
-        try:
-            con.execute(q)
-            #Get Response
-            id = con.fetchone()[0]
-            print(f"value inserted: {id}")
-        except:
-            print("At least I tried")
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
 
         for message in chats_json:
-            q = query.format('messages(idMessage, text, datetime, idUser, idChat)',"({},'{}','{}',{},{})".format(message['idMessage'],message['text'],message['datetime'],message['idUser'],message['idChat'],),'messages.idMessage')
+            q = query.format('messages(idMessage, text, datetime, idUser, idChat)','({},"{}","{}",{},{})'.format(message['idMessage'],message['text'],message['datetime'],message['idUser'],message['idChat'],),'messages.idMessage')
             print(q)
-        try:
-            con.execute(q)
-            #Get Response
-            id = con.fetchone()[0]
-            print(f"value inserted: {id}")
-        except:
-            print("At least I tried")
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
             
-    return 'Done!'
+        return print('Done!')
 
-populatetable('./chats_copy.json')
+populatetable('../chats_bike.json')
