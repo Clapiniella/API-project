ba5d63d2a9e11ee641b37a1992415b624d0298d7
Clara <clapiniella@gmail.com>
2019-11-30 21:30:51 +0100

    done for today

--- src/API.py
+++ src/API.py
@@ -1,5 +1,76 @@
-import os
-from bottle import route, run, get, error
-import pandas as pd  
-import json 
+from bottle import route, run, template, get, post
+import sqlalchemy as db
+import getpass
+import json
+import pandas as pd
+
+password = getpass.getpass("Insert your mysql root password: ")
+engine = db.create_engine('mysql+pymysql://root:{}@localhost/api-project'.format(password))
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+
+@get('idUser/<number>')
+def querita(number):
+    query = """
+        SELECT * FROM messages WHERE idUser={}
+    """.format(number)
+    print("Running query")
+    print(query)
+    df= pd.read_sql_query(query, engine)
+    jdaughter = df.to_json(orient='records')
+    return json.dumps(jdaughter)
+
+@post('/add/<jdaughter>')
+def populatetable(jdaughter):
+    query= "INSERT INTO {} VALUES {}"
+
+    with engine.connect() as con:
+
+        with open(jdaughter) as f:
+            chats_json = json.load(f)
+        
+        users = list(set([(chats_json[i]['idUser'],chats_json[i]['userName']) for i in range(len(chats_json))]))
+        chats = list(set([(chats_json[i]['idChat']) for i in range(len(chats_json))]))
+        
+        for user in users:
+            q = query.format('users (idUser, userName)',"({}, '{}')".format(user[0],user[1]),'users.idUser')
+            print(q)
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
+
+        for chat in chats:
+            q = query.format('chats(idChat)',"({})".format(chat),'chats.idChat')
+            print(q)
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
+
+        for message in chats_json:
+            q = query.format('messages(idMessage, text, datetime, idUser, idChat)','({},"{}","{}",{},{})'.format(message['idMessage'],message['text'],message['datetime'],message['idUser'],message['idChat'],),'messages.idMessage')
+            print(q)
+            try:
+                con.execute(q)
+                #Get Response
+                id = con.fetchone()[0]
+                print(f"value inserted: {id}")
+            except:
+                print("At least I tried")
+            
+        return print('Done!')
+
+
+run(host='localhost', port=8080)
+
 
---
+++ src/getSQL.py
@@ -0,0 +1,40 @@
+import sqlalchemy as db
+import getpass
+import json
+import pandas as pd
+
+#https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+
+password = getpass.getpass("Insert your mysql root password: ")
+engine = db.create_engine('mysql+pymysql://root:{}@localhost/api-project'.format(password))
+print("Connected to server!")
+
+def querita(number):
+    query = """
+        SELECT * FROM messages WHERE idUser={}
+    """.format(number)
+    print("Running query")
+    print(query)
+    df= pd.read_sql_query(query, engine)
+    jdaughter = df.to_json(orient='records')
+    return json.loads(jdaughter)
+    
+
+querita(8)
+
+'''
+
+password = getpass.getpass("Insert your mysql root password: ")
+engine = db.create_engine('mysql+pymysql://root:{}@localhost/api-project'.format(password))
+connection = engine.connect()
+metadata = db.MetaData()
+chat = db.Table('messages', metadata, autoload=True, autoload_with=engine)
+query = db.select([chat])
+
+ResultProxy = connection.execute(query)
+
+ResultSet = ResultProxy.fetchall()
+
+print(json.dumps(ResultSet[:3],indent=4))
+
+'''
