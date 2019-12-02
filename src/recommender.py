5b6a5039bc607c95dea4ba3ff363eaf2279c0afc
Clara <clapiniella@gmail.com>
2019-12-02 20:01:07 +0100

    almost

--- src/recommender.py
+++ src/recommender.py
@@ -10,37 +10,41 @@ def getPhrase(chats):
     analiza = ' '.join([word for frase in chats for word in frase])
     return analiza
 
-users= list(sql.getUsers())
 
-usernames=[]
-for u in users:
-    usernames.append(u[1])
-    
-usersid=[]
-for u in users:
-    usersid.append(u[0])
+def makeDict():
+    users= list(sql.getUsers())
+    usernames=[]
+    for u in users:
+        usernames.append(u[1])
+    usersid=[]
+    for u in users:
+        usersid.append(u[0])
+    whattheysaid=[]
+    for i in usersid:
+        whattheysaid.append(sql.getChatIdUser(i))
+    otherlist=[]
+    for e in whattheysaid:
+        otherlist.append(getPhrase(e))
+    docs={}
+    for i,nombre in enumerate(usernames): 
+        docs[nombre]= otherlist[i]
+    return docs
 
-whattheysaid=[]
-for i in usersid:
-    whattheysaid.append(sql.getChatIdUser(i))
 
-otherlist=[]
-for e in whattheysaid:
-    otherlist.append(getPhrase(e))
+def recommendator(name, dictionary):
+    count_vectorizer = CountVectorizer()
+    sparse_matrix = count_vectorizer.fit_transform(dictionary.values())
+    doc_term_matrix = sparse_matrix.todense()
+    df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names(), index=dictionary.keys())
+    similarity_matrix = distance(df, df)
+    sim_df = pd.DataFrame(similarity_matrix, columns=dictionary.keys(), index=dictionary.keys())
+    np.fill_diagonal(sim_df.values, 0) 
+    pepe = sim_df.idxmax()
+    dict45825121={}
+    dict45825121[name]= 'Your best friend should be {}'.format(pepe.loc[name])
+    return dict45825121
 
-docs={}
-for i,nombre in enumerate(usernames): 
-    docs[nombre]= otherlist[i]
-
-docs.values()
-count_vectorizer = CountVectorizer()
-sparse_matrix = count_vectorizer.fit_transform(docs.values())
-doc_term_matrix = sparse_matrix.todense()
-df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names(), index=docs.keys())
-similarity_matrix = distance(df, df)
-sim_df = pd.DataFrame(similarity_matrix, columns=docs.keys(), index=docs.keys())
-np.fill_diagonal(sim_df.values, 0) 
-sim_df.idxmax()
+print(recommendator('Tony Pons', makeDict()))
 
 
 '''
